5c5264b6d27a53e66f75a53b73136802ad706fa6
Russell Gordon <rgordon@lcs.on.ca>
2023-05-23 14:56:44 -0400

    Got the picture, headline, and new source mostly organized.

--- AppleNewsPlus/Views/NewsFeedView.swift
+++ AppleNewsPlus/Views/NewsFeedView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct NewsFeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Image("Farming")
+                .resizable()
+                .scaledToFit()
+            
+            Image("GlobeAndMail")
+                .resizable()
+                .scaledToFit()
+
+            Text("Maritime farmers holding breath as record-dry spring wings region")
+                .font(.title)
+                .fontWeight(.semibold)
+        }
     }
 }
 
