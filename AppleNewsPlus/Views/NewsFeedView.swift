1f957fadd2796788889d26c0ee41c09d02fe9e68
Russell Gordon <rgordon@lcs.on.ca>
2023-05-23 15:13:59 -0400

    Got basic layout of a news item figured out. Color, shape, and so on.

--- AppleNewsPlus/Views/NewsFeedView.swift
+++ AppleNewsPlus/Views/NewsFeedView.swift
@@ -21,7 +21,19 @@ struct NewsFeedView: View {
             Text("Maritime farmers holding breath as record-dry spring wings region")
                 .font(.title)
                 .fontWeight(.semibold)
+                .padding(.horizontal)
+            
+            Divider()
+            
+            HStack {
+                Text("1h ago")
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .padding([.horizontal, .bottom])
         }
+        .background(Color("DarkGrey"))
+        .cornerRadius(25.0)
     }
 }
 
