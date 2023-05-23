2e7ea6433309aa671b6c4b554f02797dd4faf1b7
Russell Gordon <rgordon@lcs.on.ca>
2023-05-23 14:39:46 -0400

    Organized project by added Views group and news feed view.

---
+++ AppleNewsPlus/Views/NewsFeedView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsFeedView.swift
+//  AppleNewsPlus
+//
+//  Created by Russell Gordon on 2023-05-23.
+//
+
+import SwiftUI
+
+struct NewsFeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsFeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsFeedView()
+    }
+}
