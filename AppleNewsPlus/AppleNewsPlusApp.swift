a2d05d9096aaf79dac78597ff885750cbdbab1ba
Russell Gordon <rgordon@lcs.on.ca>
2023-05-23 14:36:19 -0400

    Initial Commit

---
+++ AppleNewsPlus/AppleNewsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsPlusApp.swift
+//  AppleNewsPlus
+//
+//  Created by Russell Gordon on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
